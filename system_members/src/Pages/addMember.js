2bff393d41d69257534b5081f7822331428dd66f
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-24 16:40:47 +0200

    Some updates in my system

--- system_members/src/Pages/addMember.js
+++ system_members/src/Pages/addMember.js
@@ -7,13 +7,14 @@ const API_URL = 'http://127.0.0.1:8000/api/addMember';
 
 function addMember() {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
+    const [Name, setName] = useState('');
+    const [Surname, setSurname] = useState('');
+    const [Task, setTask] = useState('');
 
     const handleSubmit = event => {
         event.preventDefault();
 
-        axios.post(`${API_URL}users/`, { name, email })
+        axios.post(`${API_URL}users/`, { Name, Surname, Task })
             .then(response => {
                 console.log(response.data);
             });
@@ -22,18 +23,18 @@ function addMember() {
     return (
         <>
             <div className="container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label for="exampleInputEmail1" className="form-label">Nome</label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" placeholder="Preencha com seu nome" />
+                        <input type="text" className="form-control" value={Name} onChange={event => setName(event.target.value)} placeholder="Preencha com seu nome" />
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Apelido</label>
-                        <input type="surname" className="form-control" id="exampleInputPassword1" placeholder="Preencha com seu apelido" />
+                        <input type="surname" className="form-control" value={Surname} onChange={event => setSurname(event.target.value)} placeholder="Preencha com seu apelido" />
                     </div>
                     <div className="mb-3">
                         <label for="exampleInputPassword1" className="form-label">Tarefa</label>
-                        <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Preencha com suas tarefas" />
+                        <input type="text" className="form-control" value={Task} onChange={event => setTask(event.target.value)} placeholder="Preencha com suas tarefas" />
                     </div>
                     <button type="submit" className="btn btn-primary">Adicionar</button>
                 </form>
