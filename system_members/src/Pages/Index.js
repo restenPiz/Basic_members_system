e5157ea3142df5a8610e7d58326e7dcbb6ed5ad4
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-24 15:59:54 +0200

    Some updates in my Index.js

--- system_members/src/Pages/Index.js
+++ system_members/src/Pages/Index.js
@@ -3,16 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 function Index() {
+
+    const estilos={
+        textDecoration: "none",
+        color:"white",
+        listStyleType:"none",
+    }
+
     return (
         <>
             <div className="container">
 
                 <button type="button" class="btn btn-success">
-                    <a href="/AddMember">Adicionar Membro</a>
+                    <a style={estilos} href="/AddMember">Adicionar Membro</a>
                 </button><br /><br />
 
                 <button type="button" class="btn btn-success">
-                    <a href="/AddMember">Adicionar Membro</a>
+                    <a style={estilos} href="/AddMember">Adicionar Membro</a>
                 </button>
             </div>
         </>
