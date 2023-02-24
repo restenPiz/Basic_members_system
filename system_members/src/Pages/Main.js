eff925d0eaa2e7272af42f1d366798893bce67f1
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-24 17:32:17 +0200

    Update in addMember.js

--- system_members/src/Pages/Main.js
+++ system_members/src/Pages/Main.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:8000/api/';
