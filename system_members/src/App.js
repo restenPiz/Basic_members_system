beb096ab1b06ec469f903ae275ead7ff892bde36
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-24 15:35:30 +0200

    Some updates in addMember.js

--- system_members/src/App.js
+++ system_members/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './Pages/Main';
+import addMember from './Pages/addMember';
 
 function App() {
   return (
     <>
       <br/>
       <Main/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/addMember" element={<addMember/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
---
+++ system_members/src/Pages/addMember.js
@@ -0,0 +1,8 @@
+function addMember()
+{
+    return (
+        <></>
+    );
+}
+
+export default addMember;
