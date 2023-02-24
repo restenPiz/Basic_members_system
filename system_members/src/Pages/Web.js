5b1af56d83e958e8a2bd1009d0898dcf97e0802b
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-24 16:03:10 +0200

    Some updates in my web.js

--- system_members/src/Pages/Web.js
+++ system_members/src/Pages/Web.js
@@ -1,15 +1,17 @@
-import { Route, BrowserRouter,Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import AddMember from "./AddMember";
+import Main from "./Main";
 
 function Web() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/AddMember" element={<AddMember />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/AddMember" element={<AddMember />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 export default Web;
